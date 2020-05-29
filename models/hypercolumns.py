ad5d65db93be66d9c0eba0adbf717f173f6d6a6f
Steven Basart <xksteven@users.noreply.github.com>
2020-05-28 20:23:37 -0400

    Creating a class for hypercolumns

---
+++ models/hypercolumns.py
@@ -0,0 +1,70 @@
+import numpy as np
+import torch
+import torch.nn as nn
+
+
+def calculate_hyper_indices(in_planes, out_planes, prev_indices):
+    _, _, in_h, in_w = in_planes.size()
+    _, _, out_h, out_w = out_planes.size()
+    rows, cols = prev_indices
+    # center the rows (or cols), then scale, then back to 0 to height (or width)
+    new_rows = ((rows - in_h//2) * (out_h/in_h) + out_h//2).astype(np.int)
+    new_cols = ((cols - in_w//2) * (out_w/in_w) + out_w//2).astype(np.int)
+    return (new_rows, new_cols)
+
+def get_random_indices():
+    out_size = (output_shape[0]*output_shape[1], 2)
+    indices = sorted(np.random.uniform(high=x.size()[2], size=out_size))
+
+class hyper_columns():
+
+    def __init__(self):
+        if indices == None:
+            indices = get_random_indices()
+        self.indices = indices
+
+    # be able to pass in a shape for hypercols
+    # check if shape is bigger than any requested layer
+    # scale layer to appropriate dimension and take full layer
+    # Need option for full hypercolumn
+
+
+    # this should work layer by layer
+    # take in indices and return hyper column
+
+    def get_hypercolumn():
+       if self.sampling:
+           if indices == None:
+           indices_list = [indices]
+
+           hypercols = [x1, x6, x7, x8]
+
+           # calculate the indices for each layer
+           for index, l in enumerate(hypercols):
+               if (index == (len(hypercols) - 1)):
+                   break
+               prev_indices = indices_list[-1]
+               in_planes = hyper_cols[index]
+               out_planes = hyper_cols[index+1]
+               indices_list.append(calculate_hyper_indices(in_planes, out_planes, prev_indices))
+
+           for index, l in enumerate(hypercols):
+               rows, cols = indices_list[index]
+               hypercols[index] = hypercols[index][:,:,rows,cols]
+
+           hypercols = torch.cat(hypercols, dim=1)
+
+       # else we take the full feature maps as our hypercolumns
+       else:
+           hypercols = [x1, x6, x7, x8]
+           for index, l in enumerate(hypercols):
+               hypercols[index] = nn.functional.interpolate(l, output_shape, mode= interp_mode)
+
+
+    def cat_features():
+        return torch.cat(x, dim=1)
+
+    # The main method should take in a list of the features and return the hypercolumns
+    def create_hypercolumn(x):
+
+
